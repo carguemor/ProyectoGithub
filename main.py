50a01d062abebcfd1363001af4f4cd28f25731cd
carguemor <carlos0706xiiigmc@uma.es>
2022-12-12 10:43:40 +0100

    Prueba mas codigo

--- main.py
+++ main.py
@@ -14,4 +14,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+    for i in range(0,5):
+        print("Hello: ", i)
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
